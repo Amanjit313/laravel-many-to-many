a5ae896014f7d00c535cdf19e0e2f97012c15477
Amanjit313 <amanjitsingh313@gmail.com>
2022-07-11 18:10:18 +0200

    creata la seeder comune fra i miei du elementi posts e tags e popolata

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(PostsTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
         $this->call(FkTableSeeder::class);
+        $this->call(TagsTableSeeder::class);
     }
 }
